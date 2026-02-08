cc0c9d12508113bb60f9a1db7c67850ccf7e6729
Sarthak69g <malaichaap3@gmail.com>
2026-02-08 18:13:27 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,21 @@
+-- Total sales by region
+SELECT region, SUM(sales) AS total_sales
+FROM sales
+GROUP BY region;
+
+-- Profit by category
+SELECT category, SUM(profit) AS total_profit
+FROM sales
+GROUP BY category;
+
+-- Top 5 highest sales orders
+SELECT order_id, sales
+FROM sales
+ORDER BY sales DESC
+LIMIT 5;
+
+-- Average profit per region
+SELECT region, AVG(profit) AS avg_profit
+FROM sales
+GROUP BY region;
 
